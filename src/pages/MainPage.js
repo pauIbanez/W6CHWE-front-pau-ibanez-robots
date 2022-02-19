800f3e97cd83af6dcf3856d4a1da63609b1b2227
Pau Ibáñez <pauibanez2001@gmail.com>
2022-02-19 19:04:47 +0100

    added presentation video

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,6 +1,21 @@
+import styled from "styled-components";
+
+const Video = styled.video`
+  width: 100vw;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 0;
+`;
+
 const MainPage = () => {
   return (
     <>
+      <header>
+        <Video autoPlay muted loop>
+          <source src="./videos/presentation.mp4" type="video/mp4" />
+        </Video>
+      </header>
       <h1>Main page</h1>
     </>
   );
