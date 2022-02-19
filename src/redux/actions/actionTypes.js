ea06cfebfeef738ee5b6fdd8730abb08baf37924
Pau Ibáñez <pauibanez2001@gmail.com>
2022-02-19 16:55:59 +0100

    added actionTypes

---
+++ src/redux/actions/actionTypes.js
@@ -0,0 +1,9 @@
+const actionTypes = {
+  loadRobots: "load-robots",
+  updateRobot: "update-robot",
+  addRobot: "create-robot",
+  replaceRobot: "replace-robot",
+  deleteRobot: "delete-robot",
+};
+
+export default actionTypes;
