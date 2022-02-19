9ad5ea24966c31de06f156becd59852c4cb125b8
Pau Ibáñez <pauibanez2001@gmail.com>
2022-02-19 17:18:15 +0100

    added cases for reducer

--- src/redux/reducers/robotsReducer.js
+++ src/redux/reducers/robotsReducer.js
@@ -1,7 +1,32 @@
+import actionTypes from "../actions/actionTypes";
+
 const robotsReducer = (currentRobots = [], action = {}) => {
   let newRrobots;
 
   switch (action.type) {
+    case actionTypes.loadRobots:
+      newRrobots = [...action.robots];
+      break;
+
+    case actionTypes.updateRobot:
+      newRrobots = currentRobots.map((robot) =>
+        robot.id === action.robot.id ? action.robot : robot
+      );
+      break;
+
+    case actionTypes.addRobot:
+      newRrobots = [...currentRobots, action.robot];
+      break;
+
+    case actionTypes.replaceRobot:
+      newRrobots = currentRobots.map((robot) =>
+        robot.id === action.robot.id ? action.robot : robot
+      );
+      break;
+
+    case actionTypes.deleteRobot:
+      newRrobots = currentRobots.filter(({ id }) => id !== action.id);
+      break;
     default:
       newRrobots = [...currentRobots];
   }
