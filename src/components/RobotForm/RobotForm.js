0d987619ce8613b98061dafccca62a9c8558791a
Pau Ibáñez <pauibanez2001@gmail.com>
2022-02-20 19:53:07 +0100

    added all form styles

--- src/components/RobotForm/RobotForm.js
+++ src/components/RobotForm/RobotForm.js
@@ -1,4 +1,6 @@
-import { useEffect, useRef, useState } from "react";
+import { useRef } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faXmark } from "@fortawesome/free-solid-svg-icons";
 import styled from "styled-components";
 
 const Form = styled.form`
@@ -76,6 +78,90 @@ const Image = styled.img`
   left: 0;
 `;
 
+const TagsSelector = styled.select`
+  height: 50px;
+  outline: none;
+  border-radius: 50px;
+  border: 1px solid gray;
+  padding-left: 30px;
+  width: 300px;
+  font-size: 15px;
+
+  &:focus {
+    border: 2px solid gray;
+    border-radius: 25px 25px 0 0;
+  }
+`;
+
+const TagThingy = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
+
+const TagsShower = styled.div`
+  height: 50px;
+  border-radius: 10px;
+  background-color: lightgray;
+  width: 545px;
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  padding: 10px;
+  margin-top: 20px;
+`;
+
+const Tag = styled.div`
+  padding: 7px;
+  background-color: purple;
+  border-radius: 5px;
+  width: fit-content;
+  height: 30px;
+  color: white;
+  display: flex;
+  align-items: center;
+  gap: 3px;
+`;
+
+const TagButton = styled.button`
+  background-color: transparent;
+  border: none;
+  font-family: inherit;
+  font-size: inherit;
+  color: white;
+  display: flex;
+  justify: content: center;
+  align-items: center;
+`;
+
+const FormFinal = styled.div`
+  display: flex;
+  height: 300px;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const SubmitButton = styled.button`
+  width: 150px;
+  height: 50px;
+  background-color: #f8f9fb;
+  border: 1px solid gray;
+  border-radius: 50px;
+`;
+
+const FormRow = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const ForemCol = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 30px;
+`;
+
 const RobotForm = ({ formData, setFormData, onSubmit }) => {
   const localImageUrl = useRef(formData.image);
 
@@ -90,6 +176,47 @@ const RobotForm = ({ formData, setFormData, onSubmit }) => {
     setFormData(newFormData);
   };
 
+  const assesTag = (event) => {
+    if (formData.tags.includes(event.target.value)) {
+      const newTags = formData.tags.filter((tag) => tag !== event.target.value);
+      const newFormData = {
+        ...formData,
+        tags: newTags,
+      };
+      setFormData(newFormData);
+    } else {
+      const newTags = [...formData.tags, event.target.value];
+      const newFormData = {
+        ...formData,
+        tags: newTags,
+      };
+      setFormData(newFormData);
+    }
+  };
+
+  const deleteTag = (tag) => {
+    const mockEvent = {
+      target: {
+        value: tag,
+      },
+    };
+    assesTag(mockEvent);
+  };
+
+  const tagsToRender = formData.tags.map((tag) => (
+    <Tag key={tag}>
+      {tag[0].toUpperCase() + tag.substring(1)}
+      <TagButton
+        onClick={(event) => {
+          event.preventDefault();
+          deleteTag(tag);
+        }}
+      >
+        <FontAwesomeIcon icon={faXmark} />
+      </TagButton>
+    </Tag>
+  ));
+
   return (
     <>
       <h2>Upload robot form</h2>
@@ -103,6 +230,7 @@ const RobotForm = ({ formData, setFormData, onSubmit }) => {
             value={formData.name}
             onChange={updateData}
             autoComplete="off"
+            placeholder="Robot name..."
           />
         </InputThingy>
         <InputThingy>
@@ -114,45 +242,71 @@ const RobotForm = ({ formData, setFormData, onSubmit }) => {
             value={formData.description}
             onChange={updateData}
             autoComplete="off"
+            placeholder="Robot description..."
           />
         </InputThingy>
-        <InputThingy>
-          <InputName htmlFor="universe">Universe</InputName>
-          <TextInput
-            type="text"
-            name="universe"
-            id="universe"
-            value={formData.universe}
-            onChange={updateData}
-            autoComplete="off"
-          />
-        </InputThingy>
-        <InputThingy>
-          <InputName htmlFor="image">Image</InputName>
-          <TextInput
-            type="text"
-            name="image"
-            id="image"
-            value={formData.image}
-            onChange={updateData}
-            autoComplete="off"
-          />
-        </InputThingy>
-        <ImageHolder>
-          <Image
-            src={localImageUrl.current}
-            alt="Robot"
-            height="360"
-            width="350"
-            onError={(event) => {
-              event.target.style.display = "none";
-            }}
-            onLoad={(event) => {
-              event.target.style.display = "block";
-            }}
-          />
-          <ImagePlaceHolder>Input a valid image url</ImagePlaceHolder>
-        </ImageHolder>
+
+        <FormRow>
+          <ForemCol>
+            <InputThingy>
+              <InputName htmlFor="image">Image</InputName>
+              <TextInput
+                type="text"
+                name="image"
+                id="image"
+                value={formData.image}
+                onChange={updateData}
+                autoComplete="off"
+                placeholder="Image url..."
+              />
+            </InputThingy>
+            <ImageHolder>
+              <Image
+                src={localImageUrl.current}
+                alt="Robot"
+                height="360"
+                width="350"
+                onError={(event) => {
+                  event.target.style.display = "none";
+                }}
+                onLoad={(event) => {
+                  event.target.style.display = "block";
+                }}
+              />
+              <ImagePlaceHolder>Input a valid image</ImagePlaceHolder>
+            </ImageHolder>
+          </ForemCol>
+
+          <ForemCol>
+            <InputThingy>
+              <InputName htmlFor="universe">Universe</InputName>
+              <TextInput
+                type="text"
+                name="universe"
+                id="universe"
+                value={formData.universe}
+                onChange={updateData}
+                autoComplete="off"
+                placeholder="Robot universe..."
+              />
+            </InputThingy>
+            <TagThingy>
+              <InputName htmlFor="image">Tags</InputName>
+              <TagsSelector name="tags" id="tags" value="" onChange={assesTag}>
+                <option value="">Select a Tag</option>
+                <option value="real">Real</option>
+                <option value="sentient">Sentient</option>
+                <option value="humanoid">Humanoid</option>
+                <option value="lifeLike">Life-like</option>
+                <option value="spaceCraft">Space Craft</option>
+              </TagsSelector>
+              <TagsShower>{tagsToRender}</TagsShower>
+            </TagThingy>
+          </ForemCol>
+        </FormRow>
+        <FormFinal>
+          <SubmitButton> Create! </SubmitButton>
+        </FormFinal>
       </Form>
     </>
   );
