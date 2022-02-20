fbb8f4a1532bf4f2fa276d1d433f8b8482a0912b
Pau Ibáñez <pauibanez2001@gmail.com>
2022-02-20 11:29:25 +0100

    added tests to resultHandlers

---
+++ src/utils/apiResultsHandlers.test.js
@@ -0,0 +1,59 @@
+import actionTypes from "../redux/actions/actionTypes";
+import { getAllRobotsApiHandler } from "./apiResultsHandlers";
+
+describe("Given getAllRobotsApiHandler", () => {
+  describe("When it's invoked with a dispatch and a result", () => {
+    test("Then it should call dispatch with a loadRobotsAction with the robots", () => {
+      const mockDispatch = jest.fn();
+      const robots = [
+        {
+          id: 1,
+          name: "Robot 1",
+          desctiption: "This a robot",
+        },
+        {
+          id: 2,
+          name: "Robot 2",
+          desctiption: "This a robot",
+        },
+        {
+          id: 3,
+          name: "Robot 3",
+          desctiption: "This a robot",
+        },
+      ];
+
+      const result = {
+        ok: true,
+        body: {
+          robots,
+        },
+      };
+
+      const expectedAction = {
+        type: actionTypes.loadRobots,
+        robots,
+      };
+
+      const resultHandler = getAllRobotsApiHandler(mockDispatch);
+      resultHandler(result);
+
+      expect(mockDispatch).toHaveBeenCalledWith(expectedAction);
+    });
+  });
+
+  describe("When it's invoked with a dispatch and a result as not ok", () => {
+    test("Then it should not call dispatch", () => {
+      const mockDispatch = jest.fn();
+
+      const result = {
+        ok: false,
+      };
+
+      const resultHandler = getAllRobotsApiHandler(mockDispatch);
+      resultHandler(result);
+
+      expect(mockDispatch).not.toHaveBeenCalled();
+    });
+  });
+});
